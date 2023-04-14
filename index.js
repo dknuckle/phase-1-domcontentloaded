774e07410863c4344f1e8653df6cf7f86807e24c
Dalton Knuckles <djck244@gmail.com>
2023-04-14 09:19:16 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let ct = document.getElementById('text');
+    return ct.innerHTML = "This is really cool!";
+  });
+  
